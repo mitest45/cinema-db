77c0c4a7d5407d2b3916b1e17654a47479e1c5d0
mitest45 <125724129+mitest45@users.noreply.github.com>
2025-11-20 20:34:43 +0530

    Admin Controller Stub Command

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+
+class AuthController extends AdminController
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
